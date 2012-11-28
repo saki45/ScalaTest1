564290d6e7366af0ad295477aa4f9cfed1f13697
Shaoyu Qi <Shaoyu Qi@sm445-04>
2012-11-27 19:03:34 -0500

    8Queens

--- src/scalaTest1/Test1.scala
+++ src/scalaTest1/Test1.scala
@@ -4,7 +4,11 @@ import scala.Math._
 
 object Test1 {
 	def main(args: Array[String]) :Unit = {
-	  print(mySqrt(22))
+	  println(mySqrt(22))
+	  
+	  val ans = queens(8)
+	  for(i <- ans)
+	    println(i)
 	}
 	
 	def mySqrt(x: Double): Double = {
@@ -23,4 +27,27 @@ object Test1 {
 	  
 	  sqrtIter(1.0)
 	}
+	
+  def queens(n: Int): List[List[(Int, Int)]] = {
+    def placeQueens(k: Int): List[List[(Int, Int)]] =
+      if (k == 0) 
+        List(List())
+      else 
+        for {
+          queens <- placeQueens(k - 1)
+          column <- 1 to n
+          queen = (k, column)
+          if isSafe(queen, queens) 
+        } yield queen :: queens
+
+      def isSafe(queen: (Int, Int), queens: List[(Int, Int)]) = 
+      	queens forall (q => !inCheck(queen, q))
+
+	  def inCheck(q1: (Int, Int), q2: (Int, Int)) = 
+	    q1._1 == q2._1 ||  // same row
+	    q1._2 == q2._2 ||  // same column
+	    (q1._1 - q2._1).abs == (q1._2 - q2._2).abs // on diagonal
+        
+    placeQueens(n)
+  }	
 }
